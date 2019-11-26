61447c64a895e0a1d37698afdd15b1a694f26eee
Dzmitry_Potenko <Dzmitry_Potenko@epam.com>
2019-11-26 20:45:36 +0300

    add init script

---
+++ src/main/resources/init.sql
@@ -0,0 +1,71 @@
+create schema if not exists principal collate utf8mb4_0900_ai_ci;
+
+create table if not exists course
+(
+	name varchar(256) not null,
+	category varchar(256) null,
+	description text null,
+	id bigint auto_increment
+		primary key
+);
+
+create table if not exists lesson
+(
+	id bigint auto_increment
+		primary key,
+	lesson_text text null,
+	name varchar(256) null,
+	order_number int null,
+	video_link varchar(1000) null,
+	course_id bigint null,
+	constraint lesson_course_id_fk
+		foreign key (course_id) references course (id)
+);
+
+create table if not exists attachment
+(
+	id bigint auto_increment
+		primary key,
+	attachment_link varchar(1000) null,
+	attachment_title varchar(256) null,
+	lesson_id bigint null,
+	constraint attachment_lesson_id_fk
+		foreign key (lesson_id) references lesson (id)
+);
+
+create table if not exists test
+(
+	id bigint auto_increment
+		primary key,
+	name varchar(256) null,
+	order_number int null,
+	course_id bigint null,
+	constraint test_course_id_fk
+		foreign key (course_id) references course (id)
+);
+
+create table if not exists question
+(
+	question_text text null,
+	type varchar(50) null,
+	id bigint auto_increment
+		primary key,
+	test_id bigint null,
+	constraint question_test_id_fk
+		foreign key (test_id) references test (id)
+);
+
+create table if not exists variant
+(
+	id bigint auto_increment
+		primary key,
+	ticked tinyint(1) null,
+	wrong tinyint(1) null,
+	`right` tinyint(1) null,
+	variant_text varchar(256) null,
+	explanation text null,
+	question_id bigint null,
+	constraint variant_question_id_fk
+		foreign key (question_id) references question (id)
+);
+
