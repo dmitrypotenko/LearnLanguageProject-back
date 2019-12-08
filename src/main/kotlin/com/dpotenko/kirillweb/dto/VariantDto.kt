18ca110134a2e1c058f0148f7bab94e8795de3a5
Dzmitry_Potenko <Dzmitry_Potenko@epam.com>
2019-12-08 13:23:07 +0300

    move to maven

--- src/main/kotlin/com/dpotenko/kirillweb/dto/VariantDto.kt
+++ src/main/kotlin/com/dpotenko/kirillweb/dto/VariantDto.kt
@@ -1,9 +1,16 @@
 package com.dpotenko.kirillweb.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
+
 data class VariantDto(
         val variant: String,
+        @param:JsonProperty("isRight")
+        @get:JsonProperty("isRight")
         val isRight: Boolean,
+        @get:JsonProperty("isWrong")
         val isWrong: Boolean,
+        @get:JsonProperty("isTicked")
         val isTicked: Boolean,
         var id: Long?
 )
